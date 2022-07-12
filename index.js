64bad53dafc26c4c67dbcf508d7acc93c71574b2
Lalit-kumar001 <96826866+Lalit-kumar001@users.noreply.github.com>
2022-07-12 14:00:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ console.log("hi");
 var touchsurface = document.getElementById('touchsurface'),
 startX,
 startY,
-dist,
-threshold = -150, //required min distance traveled to be considered swipe
+distX,
+distY,
+threshold = 150, //required min distance traveled to be considered swipe
 allowedTime = 300, // maximum time allowed to travel that distance
 elapsedTime,
 startTime
 
 function handleswipe(isrightswipe){
-if (isrightswipe)
+if (isrightswipe=='left')
     touchsurface.innerHTML = 'Congrats, you\'ve made a <span style="color:red">right swipe!</span>'
-else{
-    touchsurface.innerHTML = 'Condition for right swipe not met yet'
+else if(swiperightBol=='right'){
+    touchsurface.innerHTML = 'Condition for right swipe met';
 }
 }
 
 document.addEventListener('touchstart', function(e){
 touchsurface.innerHTML = ''
 var touchobj = e.changedTouches[0]
-dist = 0
+distX = 0
 startX = touchobj.pageX
 startY = touchobj.pageY
 startTime = new Date().getTime() // record time when finger first makes contact with surface
@@ -32,10 +33,15 @@ e.preventDefault() // prevent scrolling when inside DIV
 
 document.addEventListener('touchend', function(e){
 var touchobj = e.changedTouches[0]
-dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+distX = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
 elapsedTime = new Date().getTime() - startTime // get time elapsed
 // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
-var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+if (elapsedTime <= allowedTime){ // first condition for awipe met
+    if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+        swiperightBol = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+    }
+}
 handleswipe(swiperightBol)
 e.preventDefault()
 }, false)
